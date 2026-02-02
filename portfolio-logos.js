a5a6d0cb98eb9a5a61e08eb0a67651c397915011
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2026-02-02 11:24:26 +0100

    Update portfolio-logos.js

--- portfolio-logos.js
+++ portfolio-logos.js
@@ -8,27 +8,39 @@ document.addEventListener("DOMContentLoaded", function() {
         {
             name: "Doners Kebab",
             url: "https://www.facebook.com/DonersLomza",
-            img: "https://ziggy83pl.github.io/rentmaster/image/logo/doners_kebab.png",
+            img: "https://ziggy83pl.github.io/zasoby/logo/doners_kebab.webp",
             title: "Doners Kebab Łomża"
         },
         {
             name: "Prodom",
             url: "https://ziggy83pl.github.io/Prodom-budownictwo/",
-            img: "https://ziggy83pl.github.io/rentmaster/image/logo/prodom.webp",
+            img: "https://ziggy83pl.github.io/zasoby/logo/prodom.webp",
             title: "PRODOM BUDOWNICTWO"
         },
         {
             name: "Siepomaga",
             url: "https://www.siepomaga.pl",
-            img: "https://ziggy83pl.github.io/rentmaster/image/logo/siepomaga.png",
+            img: "https://ziggy83pl.github.io/zasoby/logo/siepomaga.webp",
             title: "Wspieramy Siepomaga.pl"
         },
         {
             name: "Paweł Szczęsny",
             url: "https://ziggy83pl.github.io/PawelSzczesny/index.html",
-            img: "https://ziggy83pl.github.io/rentmaster/image/logo/pawel.png",
+            img: "https://ziggy83pl.github.io/zasoby/logo/pawel.webp",
             title: "CZŁOWIEK, KTÓRY WYKONA WSZYSTKO"
-        }
+        },
+         {
+            name: "Enterprise",
+            url: "https://ziggy83pl.github.io/PawelSzczesny/index.html",
+            img: "https://ziggy83pl.github.io/zasoby/logo/enterprise.webp",
+            title: "CZŁOWIEK, KTÓRY WYKONA WSZYSTKO"
+        },
+         {
+            name: "Rentmaster",
+            url: "https://ziggy83pl.github.io/PawelSzczesny/index.html",
+            img: "https://ziggy83pl.github.io/zasoby/logo/rentmaster.webp",
+            title: "CZŁOWIEK, KTÓRY WYKONA WSZYSTKO"
+        },
     ];
 
     const currentUrl = window.location.href;
@@ -51,4 +63,5 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     container.innerHTML = html;
+
 });
